15b209d3fa325104ac344838063179c874fcaade
Disanayaka AAC <charair742@gmail.com>
2023-05-24 00:12:16 +0530

    Platforms

---
+++ src/Components/AllnOne.jsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import {AiOutlineCheck} from 'react-icons/ai';
+
+const AllnOne = () => {
+  return (
+    <div name='platform' className="w-full mt-24">
+      <div className="max-w-[1240px] mx-auto px-2">
+        <h1 className="font-bold text-5xl text-center">All-In-One Platform</h1>
+        <p className="text-2xl py-8 text-gray-500 text-center">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, eum illum perspiciatis velit dolor ipsa, labore
+          veniam ullam mollitia voluptate eius quo totam facere saepe dignissimos nostrum molestiae!
+        </p>
+      </div>
+
+      <div className="grid sm:grid-cols-2  lg:grid-cols-4 gap-4 pt-4">
+        <div className="flex">
+          <div>
+            <AiOutlineCheck size={30} className="w-20 text-green-600" />
+          </div>
+          <div>
+            <h3 className="font-bold text-lg">Notifications</h3>
+            <p className="text-lg pt-2 pb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus inventore est libero nam tempora dicta quod non.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex">
+          <div>
+            <AiOutlineCheck size={30} className="w-20 text-green-600" />
+          </div>
+          <div>
+            <h2 className="font-bold text-lg">Notifications</h2>
+            <p className="text-lg pt-2 pb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus inventore est libero nam tempora dicta quod non.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex">
+          <div>
+            <AiOutlineCheck size={30} className="w-20 text-green-600" />
+          </div>
+          <div>
+            <h2 className="font-bold text-lg">Notifications</h2>
+            <p className="text-lg pt-2 pb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus inventore est libero nam tempora dicta quod non.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex">
+          <div>
+            <AiOutlineCheck size={30} className="w-20 text-green-600" />
+          </div>
+          <div>
+            <h2 className="font-bold text-lg">Notifications</h2>
+            <p className="text-lg pt-2 pb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus inventore est libero nam tempora dicta quod non.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex">
+          <div>
+            <AiOutlineCheck size={30} className="w-20 text-green-600" />
+          </div>
+          <div>
+            <h2 className="font-bold text-lg">Notifications</h2>
+            <p className="text-lg pt-2 pb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus inventore est libero nam tempora dicta quod non.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex">
+          <div>
+            <AiOutlineCheck size={30} className="w-20 text-green-600" />
+          </div>
+          <div>
+            <h2 className="font-bold text-lg">Notifications</h2>
+            <p className="text-lg pt-2 pb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus inventore est libero nam tempora dicta quod non.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex">
+          <div>
+            <AiOutlineCheck size={30} className="w-20 text-green-600" />
+          </div>
+          <div>
+            <h2 className="font-bold text-lg">Notifications</h2>
+            <p className="text-lg pt-2 pb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus inventore est libero nam tempora dicta quod non.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex">
+          <div>
+            <AiOutlineCheck size={30} className="w-20 text-green-600" />
+          </div>
+          <div>
+            <h2 className="font-bold text-lg">Notifications</h2>
+            <p className="text-lg pt-2 pb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus inventore est libero nam tempora dicta quod non.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllnOne;
